f7178112b27234472cdce18d209c1f3d0d8b1c76
AtTheGat3s <nicolas.jancke@gmx.de>
2021-04-25 14:05:20 +0200

    Add project files.

---
+++ EmptyPageRecognitionApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+
+namespace EmptyPageRecognitionApp
+{
+    class Program
+    {
+        static bool isEmptyPage(string ImageURL)
+        {
+            try
+            {
+                Bitmap image = new Bitmap(ImageURL, true);
+            }
+            catch(ArgumentException)
+            {
+                Console.Write("An error occured. The file path could be wrong.");
+            }
+            return true;
+        }
+    }
+}
